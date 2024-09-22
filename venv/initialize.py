14597ec09b873ec371cb34217fb2bda0a7dc4433
Boris-Colin <125480120+Boris-Colin@users.noreply.github.com>
2024-09-22 14:20:15 +0200

    testing

---
+++ venv/initialize.py
@@ -0,0 +1,10 @@
+import numpy as np
+def createNNetwork(nb_layers, input_size, output_size):
+
+    # The plan is to first create a neural netwok with only the input and output sizes changing.
+    # I'll make it more complex later
+
+    return 5
+
+
+
